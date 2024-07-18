58dd943ab0db37e4b7f43e2c2ac210ec560efe69
DASUKOMAL <dasuk7080@gmail.com>
2024-07-18 20:20:36 +0530

    Add files via upload

---
+++ Myself.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class Myself {
+
+	public static void main(String[] args) {
+		// write about myself
+		System.out.println("hello sir/madam, My name is Thatikonta Divya");
+		System.out.println("I am from Ramant which is located at hyderabad");
+		System.out.println("Currently i am persuing MCA at Avanthi pg college at Dilsukhnagar");
+		System.out.println("I have completed my graduation degree in Bsc computer with 7.77CGPA ");
+		System.out.println("I have done my intermediate in S>R junior college and secured 81percentage");
+		System.out.println("I have done my schooling from MFHS at erragadda");
+		System.out.println("My hobbie's are playing music watching anime and reading books");
+		System.out.println("My favourite instrument in violin");
+		System.out.println("My favourite motivational book is 'the final call Death'");
+		System.out.println("I love to watch anime like 'Onepiece','Demaon Slayer'");
+		System.out.println("coming to my family me are 7 member including my parents");
+
+	}
+
+}
