4ae9aa0da70cbba30cea2d0df64bee52078a3291
DASUKOMAL <dasuk7080@gmail.com>
2024-07-18 20:22:16 +0530

    Add files via upload
    day 1 lab about intraduction of mycomputer

---
+++ Mycomputer.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Mycomputer {
+
+	public static void main(String[] args) {
+		// Writing about My computer or laptop and its features
+				System.out.println("My laptop is hp pavilion 15.");
+				System.out.println("my laptop containing16GB ram and 512 SSD");
+				System.out.println("It is a Ultra Gaming laptop with Grapic of nvidia 4080GTR");
+				System.out.println("It has 'alexa inbuild in it");
+				System.out.println("It consiste of backlight keyborad");
+				System.out.println("Display Size -15.6 Inches and Processor - Intel Core i5-11300H (11th Gen)");
+				System.out.println("Display Feature - Full HD LED Backlit Anti-glare IPS Display");
+				System.out.println("I usually use for gaming and praticing coding in it");
+				System.out.println("its is used for multipurpose for a student");
+				System.out.println("its been more then 3 three years its don't even got any issues");
+
+	}
+
+}
